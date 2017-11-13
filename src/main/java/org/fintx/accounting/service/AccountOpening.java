765e66244ef355c12b806ff640602403ec4d061b
bluecreator <qiang.x.wang@gmail.com>
2017-11-14 01:25:33 +0800

    enable multi-account opening once

--- src/main/java/org/fintx/accounting/service/AccountOpening.java
+++ src/main/java/org/fintx/accounting/service/AccountOpening.java
@@ -19,9 +19,11 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
+import org.fintx.accounting.entity.AccountOpeningEntry;
 import org.fintx.accounting.entity.OperationEntry;
 import org.fintx.accounting.entity.Voucher;
 import org.fintx.accounting.enumeration.OperationFlag;
+import org.fintx.accounting.service.Transaction.Builder;
 import org.fintx.util.UniqueId;
 
 import lombok.Getter;
@@ -36,25 +38,24 @@ public class AccountOpening {
     private AccountOpening() {
 
     }
-    private String accountsCodeNo;
-
-    private String organizationNo;
-
-    private String customerNo;
-
-    //private String accountSn;
-
-    private String accountNo;
-
-    private String productNo;
+//    private String accountsCodeNo;
+//
+//    private String organizationNo;
+//
+//    private String customerNo;
+//
+//    //private String accountSn;
+//
+//    private String accountNo;
+//
+//    private String productNo;
     private String accountOpeningId=UniqueId.get().toString();
     
     private Voucher voucher;
 
-    private ArrayList<OperationEntry> closeAcctEntrys;
-    private ArrayList<OperationEntry> freezeEntrys;
-    private ArrayList<OperationEntry> unfreezeEntrys;
-    private ArrayList<OperationEntry> controlEntrys;
+    private ArrayList<AccountOpeningEntry> innerAccountOpeningEntrys;
+    private ArrayList<AccountOpeningEntry> customerAccountOpeningEntrys;
+    
 
     // control and buff should be a configuration not a function
     // private ArrayList controlCreditEntrys;
@@ -68,59 +69,41 @@ public class AccountOpening {
 
     public static class Builder {
 
-        private AccountOpening operation = new AccountOpening();
+        private AccountOpening accountOpening = new AccountOpening();
         private Verifer<AccountOpening> verifer = null;
-        private static final Verifer<AccountOpening> defaultVerifer = new OperationVerifer();
+        private static final Verifer<AccountOpening> defaultVerifer = new AccountOpeningVerifer();
 
+        public Builder associate(Voucher voucher) {
+            accountOpening.voucher=voucher;
+            return this;
+        }
+        
         private Builder() {
         }
         
-        public Builder authorizerNo(String authorizerNo) {
+
+        public Builder inner(String accountNo,String organizationNo, String productNo, String businessId) {
 
             return this;
         }
 
-        public Builder operateNo(String operatorNo) {
+        public Builder customer(String organizationNo, String productNo, String customerNo, String businessId) {
 
             return this;
         }
 
-        public Builder operationDate(String operatorNo) {
-
-            return this;
-        }
-
-        public Builder freeze(String acctNo, BigDecimal amt) {
-            return this;
-        }
-
-        public Builder unfreeze(String acctNo, BigDecimal amt) {
-            return this;
-        }
-
-        public Builder control(String acctNo, String accountCtrl) {
-            return this;
-        }
-
-        public Builder closeAccount(String acctNo) {
-            return this;
-        }
+     
         
         /*
          * if operationDate is different with voucher's businessDate
          */
-         public Builder operationId(String operationId) {
+         public Builder accountOpeningId(String accountOpeningId) {
          return this;
          }
         
-        /*
-         * if operationFlag is not post
-         */
-        public Builder operationFlag(OperationFlag operationFlag) {
-            return this;
-        }
+      
 
-        public Builder operationVerifer(OperationVerifer verifer) {
+        public Builder accountOpeningVerifer(Verifer verifer) {
             this.verifer = verifer;
             return this;
         }
@@ -129,10 +112,16 @@ public class AccountOpening {
             if (null == verifer) {
                 verifer = defaultVerifer;
             }
-            return operation;
+            if(verifer.verify(accountOpening)) {
+                return accountOpening; 
+            }else {
+                //TODO add special Exception
+                throw new RuntimeException("Fail to verify the accountOpening info");
+            }
+            
         };
 
-        public static class OperationVerifer implements Verifer<AccountOpening> {
+        public static class AccountOpeningVerifer implements Verifer<AccountOpening> {
 
             public boolean verify(AccountOpening txn) {
                 // acctno type length?? on the setting
