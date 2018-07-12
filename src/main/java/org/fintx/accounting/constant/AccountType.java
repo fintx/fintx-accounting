a75477611e4771e36b4edfb4988fad776a7c0668
bluecreator <qiang.x.wang@gmail.com>
2018-07-13 00:07:18 +0800

    add  new enum type

--- src/main/java/org/fintx/accounting/constant/AccountType.java
+++ src/main/java/org/fintx/accounting/constant/AccountType.java
@@ -2,11 +2,14 @@ package org.fintx.accounting.constant;
 
 import org.fintx.lang.Codeable;
 
-public enum AccountStatusEnum  implements Codeable<Integer>{
-    NORMAL(1), CLOSED(2),LOCKED(2);
+public enum AccountType implements Codeable<Integer> {
+    /**
+     * 甲 乙 丙  丁
+     */
+    A(1), B(2), C(3), D(3);
     private Integer code;
 
-    private AccountStatusEnum(Integer code) {
+    private AccountType(Integer code) {
         this.code = code;
     }
 
---
+++ src/main/java/org/fintx/accounting/constant/AccountsType.java
@@ -0,0 +1,22 @@
+package org.fintx.accounting.constant;
+
+import org.fintx.lang.Codeable;
+
+public enum AccountsType implements Codeable<Integer> {
+ // 账类
+    /**
+     * 01-对私活期 02-对公活期 03-对私定期 04-对公定期 05-拨款、贷款 06-损益 07-现金 08-其他内部帐 09-其他 99-表外
+     */
+    ONE(1), TWO(2), THREE(3);
+    private Integer code;
+
+    private AccountsType(Integer code) {
+        this.code = code;
+    }
+
+    @Override
+    public Integer getCode() {
+        // TODO Auto-generated method stub
+        return code;
+    }
+}
